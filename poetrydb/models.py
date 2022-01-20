ad6bf62cd4826d50663f243f896e5a36ea6d77d2
Lalit <lalitkumar110301@gmail.com>
2022-01-20 13:49:04 +0530

    make it better

--- poetrydb/models.py
+++ poetrydb/models.py
@@ -8,6 +8,6 @@ class Poem(models.Model):
     poemID = models.AutoField(primary_key=True)
     lang = models.CharField(max_length=15)
     publish_date = models.DateField(auto_now_add=True)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, default="")
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=1000)
