081e18448add99fbb9349be1a3ec2dc8c5ab5390
Lalit <lalitkumar110301@gmail.com>
2022-01-21 15:15:29 +0530

    delete accounts.model

--- account/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from models import *
-
-class User(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ('userid','username', 'name', 'email', 'password')
